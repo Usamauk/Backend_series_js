b9899029de6d1becdbffef480c4103f10564bc41
Usamauk <51535170+Usamauk@users.noreply.github.com>
2025-05-18 13:07:37 +0000

    utils code discussion

--- professional_project/src/index.js
+++ professional_project/src/index.js
@@ -1,11 +1,23 @@
 import dotenv from 'dotenv';
 import DB_connect from './db/index.js';
+import { app } from './app.js';
 
 dotenv.config({
     path : './env'
 })
 
 DB_connect()
+
+.then( ()=>{
+    app.listen(process.env.PORT || 8000 , ()=>{
+    console.log(`server is running ar port : , ${process.env.PORT}`);
+    
+    })
+})
+.catch( (error) =>{
+    console.log(`Mangodb connection failed !!!!`, error);
+    
+})
 
 
 
---
+++ professional_project/src/utils/asynchandler.js
@@ -0,0 +1,25 @@
+const asynchandler =  (requesthandler) => {
+    (req,res,next) =>{
+        Promise.resolve(requesthandler(req,res,next)).
+        catch((error) => next(error))
+    }
+}
+
+export {asynchandler}
+
+// const asynchandler = () => {}  // create simple function 
+// const asynchandler = (func) => () => {}
+// const asynchandler = (func) => async() => {}
+
+
+// const asynchandler = (fn) => async(req,res,next) => {
+//     try {
+//         await fn(req , res , next)
+        
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success : false,
+//             message : err.message
+//         })
+//     }
+// }
